5d2727f383f545885494c1155da164440715895c
Thelaw <Thelaw@DESKTOP-JPI6NQ5>
2021-04-19 10:24:39 -0400

    Nuevo ingreso

--- ProyectoNodeAbogado/public_html/conexion.js
+++ ProyectoNodeAbogado/public_html/conexion.js
@@ -20,6 +20,45 @@ const servidor=http.createServer((pedido,respuesta) => {
   encaminar(pedido,respuesta,camino);
 });
 
+function encaminar (pedido,respuesta,camino) {
+  
+  switch (camino) {
+    case 'web/subir': {
+      subir(pedido,respuesta);
+      break;
+    }	
+    case 'web/listadofotos': {
+      listar(respuesta);
+      break;
+    }			
+    default : {  
+      fs.stat(camino, error => {
+        if (!error) {
+            fs.readFile(camino,(error,contenido) => {
+          if (error) {
+            respuesta.writeHead(500, {'Content-Type': 'text/plain'});
+            respuesta.write('Error interno');
+            respuesta.end();					
+          } else {
+            const vec = camino.split('.');
+            const extension=vec[vec.length-1];
+            const mimearchivo=mime[extension];
+            respuesta.writeHead(200, {'Content-Type': mimearchivo});
+            respuesta.write(contenido);
+            respuesta.end();
+          }
+        });
+        } else {
+          respuesta.writeHead(404, {'Content-Type': 'text/html'});
+          respuesta.write('<!doctype html><html><head></head><body>Recurso inexistente</body></html>');		
+          respuesta.end();
+        }
+      });	
+    }
+  }	
+}
+
+
 servidor.listen(8888);
 
 
